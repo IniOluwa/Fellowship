b7ec7e38d5b93bae3d00943d2cab471f72e57cab
Ini-Oluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2020-11-21 17:11:58 +0100

    Prime Check Algorithm Modified For Better Clarity.

--- Prime/prime_check.py
+++ Prime/prime_check.py
@@ -1,12 +1,22 @@
-# Check Prime Function
-def prime(x):
-    for i in range(1, 100):
-        if x % i != 0 or x == i:
-            # Return Not Prime Message
-            return str(x) + " Is Not Prime."
+numbers = range(1, 101)
+numbers_check = range(2, 10)
 
-    # Return Prime Message
-    return str(x) + " Is A Prime Number."
+# Check Prime Function
+def prime(number):
+
+    # Check If Number Exist In List
+    if number in numbers:
+        # Loop Through Numbers Check List
+        for i in numbers_check:
+            # Check Prime Number Conditions
+            if number % i is 0 and number is not i:
+                # Return Not Prime Message
+                return str(number) + " Is Not Prime."
+            else:
+                # Return Prime Message
+                return str(number) + " Is A Prime Number."
+    else:
+        return "Your Number Is Not In The Range of Allowed Numbers At This Time."
 
 # Get User Input
 print "Check If A Number Between 1 and 100 is Prime."
