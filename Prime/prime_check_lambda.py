6720583f200cd3d967ba5525ca60d542e3381d7f
Ini-Oluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2020-11-25 11:33:14 +0100

    Created Method To Encapsulate Lambda Prime Check.

--- Prime/prime_check_lambda.py
+++ Prime/prime_check_lambda.py
@@ -1,10 +1,12 @@
-# Define List Range
-the_list = range(1, 100)
+# # Define List Range
+# the_list = range(1, 100)
+#
+# # Lambda Prime Check
+# for i in range(2, 50):
+#     the_list = filter(lambda x: x == i or x % i, the_list)
 
-# Lambda Prime Check
-for i in range(2, 50):
-    the_list = filter(lambda x: x == i or x % i, the_list)
+def prime_lambda(number):
+	return number
 
-n = input("Gimme the number: ")
-
-print n
+print "Check If Number Is Prime."
+print prime_lambda(input("Input Your Number: "))
