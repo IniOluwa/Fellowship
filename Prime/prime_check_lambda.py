13fa7aae743291ad5b0ee01a833e09333e16ea73
Ini-Oluwa Fageyinbo <inioluwafageyinbo@gmail.com>
2020-11-26 21:13:03 +0100

    Updated Comments.

--- Prime/prime_check_lambda.py
+++ Prime/prime_check_lambda.py
@@ -1,12 +1,16 @@
-# # Define List Range
-# the_list = range(1, 100)
-#
-# # Lambda Prime Check
-# for i in range(2, 50):
-#     the_list = filter(lambda x: x == i or x % i, the_list)
+# Numbers Range For Prime Check
+numbers = range(1, 101)
 
+# Numbers List To Be Checked For Finding Prime
+numbers_check = range(2, 10)
+
+# Prime Lambda Function
 def prime_lambda(number):
-	return number
+	# Lambda Prime Check
+	for num in numbers_check:
+	    prime_list = filter(lambda x: x is not num and x % num is 0, numbers)
+	return prime_list
 
-print "Check If Number Is Prime."
+# Number Input
+print "Check If Number Is Prime In The Range Of 100."
 print prime_lambda(input("Input Your Number: "))
